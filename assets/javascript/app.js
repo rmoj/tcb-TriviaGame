dbd76ae164d1d72bdd0681c0311467ee9fdd87b3
rmoj <rmoj99@gmail.com>
2018-07-10 02:36:49 -0400

    added countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,34 @@
 'use strict';
 
 $(document).ready(function() {
+  var timeLeft = 15;
+  var countdown;
+
+  function startClock() {
+    clearInterval(countdown);
+    countdown = setInterval(decrement, 1000);
+  }
+  function decrement() {
+    if (timeLeft > 0) {
+      timeLeft--;
+      $('#clock').text(timeLeft);
+    } else {
+      reset();
+    }
+  }
+  function reset() {
+    clearInterval(countdown);
+    timeLeft = 15;
+  }
+
   $('#btnStart').on('click', function() {
     toggleView();
-    console.log('clicked');
   });
 
   function toggleView() {
     $('.toggle').show();
     $('#btnStart').hide();
   }
+
+  startClock();
 });
