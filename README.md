62a769474565a70af430d53f9f41687a2e2d6df8
rmoj <rmoj99@gmail.com>
2018-07-12 23:52:38 -0400

    Update views

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tcb-TriviaGame
+# Truly Trifling Trivia
+
+Truly Trifling Trivia is a web-based trivia game built using Javascript and JQuery. This project shows the basics of event-driven programming specifically mouse click and timing events.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function() {
-  var timeLimit = 15; //constant
+  var timeLimit = 10; //constant
   var delayBetweenQuestions = 2000; //constant
   var timeLeft;
   var countdown;
@@ -15,87 +15,86 @@ $(document).ready(function() {
     {
       question:
         "According to the internet, what is the world's happiest animal?",
-      choice1: 'wallaby',
-      choice2: 'kangaroo',
-      choice3: 'koala',
-      choice4: 'quokka',
+      choice1: '(1) wallaby',
+      choice2: '(2) kangaroo',
+      choice3: '(3) koala',
+      choice4: '(4) quokka',
       answer: 4,
       fact:
         'Quokkas have a signature smile which led many to believe they are always happy.'
     },
     {
       question: 'You can find quokkas only in',
-      choice1: 'The Galapagos Islands',
-      choice2: 'Western Australia',
-      choice3: 'The Amazon rainforest',
-      choice4: 'Northern Ireland',
+      choice1: '(1) The Galapagos Islands',
+      choice2: '(2) Western Australia',
+      choice3: '(3) The Amazon rainforest',
+      choice4: '(4) Northern Ireland',
       answer: 2,
       fact:
         'Quokkas are found only in Western Australia, mostly in Rottnest Island.'
     },
     {
       question: 'The quokka is a ',
-      choice1: 'marsupial',
-      choice2: 'rodent',
-      choice3: 'bear',
-      choice4: 'cat',
+      choice1: '(1) marsupial',
+      choice2: '(2) rodent',
+      choice3: '(3) bear',
+      choice4: '(4) cat',
       answer: 1,
       fact: 'The quokka is a marsupial and a species of wallaby.'
     },
     {
       question: 'The closest relative of the quokka is the ',
-      choice1: 'wallaby',
-      choice2: 'platypus',
-      choice3: 'koala',
-      choice4: 'rat',
+      choice1: '(1) wallaby',
+      choice2: '(2) platypus',
+      choice3: '(3) koala',
+      choice4: '(4) rat',
       answer: 1,
       fact: 'The quokka is closely related to wallabies.'
     },
     {
       question: 'What do you call a baby quokka?',
-      choice1: 'quokling',
-      choice2: 'quoklet',
-      choice3: 'juoquy',
-      choice4: 'joey',
+      choice1: '(1) quokling',
+      choice2: '(2) quoklet',
+      choice3: '(3) juoquy',
+      choice4: '(4) joey',
       answer: 4,
       fact: 'A baby quokka is called a joey'
     },
     {
       question: 'Which of the following is true? A quokka is ',
-      choice1: 'nocturnal and a meat-eater',
-      choice2: 'a herbivore and is active during the day',
-      choice3: 'nocturnal and a plant-eater',
-      choice4: 'active during the day and a carnivore',
+      choice1: '(1) nocturnal and a meat-eater',
+      choice2: '(2) a herbivore and is active during the day',
+      choice3: '(3) nocturnal and a plant-eater',
+      choice4: '(4) active during the day and a carnivore',
       answer: 3,
-      fact:
-        'Quokkas are herbivores who primarily feed at night. They eat the leaves, stems, and bark of many plants in addition to grass.'
+      fact: 'Quokkas are herbivores who primarily feed at night.'
     },
     {
       question: 'Which of the following have pouches to carry their young?',
-      choice1: 'kangaroo',
-      choice2: 'wallaby',
-      choice3: 'quokka',
-      choice4: 'all of the above',
+      choice1: '(1) kangaroo',
+      choice2: '(2) wallaby',
+      choice3: '(3) quokka',
+      choice4: '(4) all of the above',
       answer: 4,
       fact:
         'Kangaroos, wallabies, and quokkas are marsupials, which means they have pouches in which they carry their young.'
     },
     {
       question: 'Which of the following is true?',
-      choice1: 'The quokka is able to go for months without a drink',
-      choice2: "The quokka's main diet consists of thin bamboo shoots",
-      choice3: 'Male quokkas constantly fight over potential mates',
-      choice4: 'Quokkas are very gentle',
+      choice1: '(1) The quokka is able to go for months without a drink',
+      choice2: "(2) The quokka's main diet consists of thin bamboo shoots",
+      choice3: '(3) Male quokkas constantly fight over potential mates',
+      choice4: '(4) Quokkas are very gentle',
       answer: 1,
       fact:
         'If necessary, quokkas can survive for long periods of time without food or water by living off the fat stored in their tails.'
     },
     {
       question: 'Which of the following is false?',
-      choice1: 'Quokkas regurgitate their food and eat it again',
-      choice2: "You can't own one",
-      choice3: 'It is illegal to touch quokkas',
-      choice4: 'Only kids are allowed to feed quokkas',
+      choice1: '(1) Quokkas regurgitate their food and eat it again',
+      choice2: "(2) You can't own one",
+      choice3: '(3) It is illegal to touch quokkas',
+      choice4: '(4) Only kids are allowed to feed quokkas',
       answer: 4,
       fact:
         "Feeding quokkas is discouraged because 'human food' can cause dehydration and malnourishment, both of which are detrimental to the quokka's health."
@@ -103,11 +102,11 @@ $(document).ready(function() {
     {
       question:
         'How do female quokkas protect their young when pursued by a predator?',
-      choice1: 'It carries it young in its pouch and runs like hell',
-      choice2: 'it leaves the baby in a safe place and then fights',
-      choice3: 'It fights fiercely with baby in pouch',
+      choice1: '(1) It carries it young in its pouch and runs like hell',
+      choice2: '(2) it leaves the baby in a safe place and then fights',
+      choice3: '(3) It fights fiercely with baby in pouch',
       choice4:
-        "They don't. It ejects the baby from its pouch and sacrifices it in order to escape.",
+        "(4) They don't. It ejects the baby from its pouch and sacrifices it in order to escape.",
       answer: 4,
       fact:
         'If a mama quokka is threatened by a predator she will often throw her baby on the ground to distract the predator and save her own life.'
@@ -115,20 +114,14 @@ $(document).ready(function() {
   ];
 
   function initQuiz() {
-    currentIndex = 9; //change this
+    currentIndex = 9;
     ansCorrect = 0;
     ansIncorrect = 0;
     ansNoAnswer = 0;
-    clearContents();
     $('.timer').show();
     $('#start').hide();
-    $('.finalScore').hide();
+    $('.scores').hide();
     displayQuestion(currentIndex);
-  }
-
-  function clearContents() {
-    $('.question').text('');
-    $('.results').text('');
   }
 
   function startClock() {
@@ -165,8 +158,9 @@ $(document).ready(function() {
 
   function displayQuestion(i) {
     if (currentIndex < arrayQuestions.length) {
+      console.log(currentIndex, arrayQuestions.length);
       $('.results').hide();
-      $('.question').show();
+      $('.quiz').show();
       $('#question').text(arrayQuestions[i].question);
       $('#choice1').text(arrayQuestions[i].choice1);
       $('#choice2').text(arrayQuestions[i].choice2);
@@ -184,14 +178,14 @@ $(document).ready(function() {
       return 'Correct!';
     } else {
       ansIncorrect++;
-      return 'Duh...Nope';
+      return 'Duh...Nope.';
     }
   }
 
   function displayScore() {
     $('.timer').hide();
     $('.results').hide();
-    $('.finalScore').show();
+    $('.scores').show();
     $('#correct').text(ansCorrect);
     $('#incorrect').text(ansIncorrect);
     $('#noanswer').text(ansNoAnswer);
@@ -199,11 +193,12 @@ $(document).ready(function() {
 
   function displayResults(result, correctAnswer) {
     if (currentIndex < arrayQuestions.length) {
-      clearContents();
-      $('.question').hide();
-      $('.results').show();
+      $('.quiz').hide();
+      $('.timer').hide();
       $('#result').text(result);
       $('#fact').text(arrayQuestions[currentIndex].fact);
+      $('.results').show();
+
       setTimeout(function() {
         currentIndex++;
         displayQuestion(currentIndex);
@@ -215,7 +210,7 @@ $(document).ready(function() {
 
   $('.choice').hover(
     function() {
-      $(this).css({ 'font-weight': 'bold', color: 'green' });
+      $(this).css({ 'font-style': 'italic', color: 'green' });
     },
     function() {
       $(this).css({ 'font-weight': 'normal', color: 'black' });
